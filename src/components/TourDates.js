f8a9fbaaf586710fa195eacf83d530483441df73
Zach Grusznsk <zachg@Zachs-MacBook-Air.local>
2019-09-14 22:33:05 -0400

    split up future and past dates

--- src/components/TourDates.js
+++ src/components/TourDates.js
@@ -41,6 +41,14 @@ const TourDatesList = styled.div`
   margin: auto;
   padding: 2rem;
 
+  .past-dates,
+  .upcoming-dates {
+    font-family: "Source Code Pro", monospace;
+    color: #444;
+    text-align: center;
+    font-weight: bold;
+  }
+
   @media only screen and (max-width: 600px) {
     padding: 0.5rem;
     width: 95%;
@@ -81,14 +89,45 @@ const TourDates = () => (
       }
     `}
     render={data => {
-      const filteredByDate = data.allWordpressWpTourDates.edges.sort((a, b) => {
-        return new Date(b.node.acf.date) - new Date(a.node.acf.date);
+      const pastDates = data.allWordpressWpTourDates.edges.filter(date => {
+        return moment(date.node.acf.date) < moment(Date.now());
       });
+      const futureDates = data.allWordpressWpTourDates.edges.filter(date => {
+        return moment(date.node.acf.date) >= moment(Date.now());
+      });
+      const filteredByDate = dates =>
+        dates.sort((a, b) => {
+          return new Date(b.node.acf.date) - new Date(a.node.acf.date);
+        });
+
+      // const filteredByDate = data.allWordpressWpTourDates.edges.sort((a, b) => {
+      //   return new Date(b.node.acf.date) - new Date(a.node.acf.date);
+      // });
+
       return (
         <TourDatesSection>
           <h2>Tour Dates</h2>
           <TourDatesList>
-            {filteredByDate.map(item => {
+            <p className="upcoming-dates">Upcoming Shows</p>
+            {filteredByDate(futureDates)
+              .reverse()
+              .map(item => {
+                const past =
+                  moment(item.node.acf.date) < moment(Date.now()) ? "past" : "";
+                return (
+                  <TourDate key={item.node.id}>
+                    <p className={past}>
+                      {moment(item.node.acf.date).format("MMM Do YYYY")}
+                    </p>
+                    <p className={past}>{item.node.acf.venue}</p>
+                    <p className={past}>{item.node.acf.city}</p>
+                  </TourDate>
+                );
+              })}
+          </TourDatesList>
+          <TourDatesList>
+            <p className="past-dates">Past Dates</p>
+            {filteredByDate(pastDates).map(item => {
               const past =
                 moment(item.node.acf.date) < moment(Date.now()) ? "past" : "";
               return (
